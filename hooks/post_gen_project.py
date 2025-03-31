f9154287af1559b37b273f7bb06abcee25782023
Mark Reuvekamp <mark.reuvekamp@rijksoverheid.nl>
2025-03-31 10:30:08 +0200

    Add the ability to add an empty Django project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,12 +2,16 @@
 import pathlib
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-    # if '{ { cookiecutter.create_author_file }}' != 'y':
-    #     pathlib.Path('AUTHORS.rst').unlink()
-    #     pathlib.Path('docs', 'authors.rst').unlink()
-    #
+    if '{{ cookiecutter.use_django }}' != 'y':
+        pathlib.Path('manage.py').unlink()
+        pathlib.Path('{{ cookiecutter.project_python_slug }}', '__main__.py').unlink()
+        pathlib.Path('{{ cookiecutter.project_python_slug }}', 'asgi.py').unlink()
+        pathlib.Path('{{ cookiecutter.project_python_slug }}', 'settings.py').unlink()
+        pathlib.Path('{{ cookiecutter.project_python_slug }}', 'urls.py').unlink()
+        pathlib.Path('{{ cookiecutter.project_python_slug }}', 'wsgi.py').unlink()
+
     # if 'no' in '\{{ cookiecutter.command_line_interface|lower }}':
     #     pathlib.Path('src', '{{ cookiecutter.project_slug }}', 'cli.py').unlink()
 
